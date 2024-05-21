f0a267d53cc51dfa24c5777e3c3c1ca4761329f3
Daniil <d.frolenko@sh.dunice.net>
2024-05-21 12:44:44 +0300

    Add IsogramValidator class

---
+++ IsogramValidation.cs
@@ -0,0 +1,23 @@
+﻿namespace IsogramValidation
+{
+    internal class IsogramValidator
+    {
+        public static bool CheckIsIsogram(string wordToCheck)
+        {
+            var wordUniqueChars = new HashSet<char>();
+            for (int i = 0; i < wordToCheck.Length; i++)
+            {
+                var currentCharInLowerCase = char.ToLower(wordToCheck[i]);
+                if (wordUniqueChars.Contains(currentCharInLowerCase))
+                {
+                    return false;
+                }
+                else
+                {
+                    wordUniqueChars.Add(currentCharInLowerCase);
+                }
+            }
+            return true;
+        }
+    }
+}
